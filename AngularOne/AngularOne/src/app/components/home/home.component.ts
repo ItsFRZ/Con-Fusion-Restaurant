6acb12b0d0905ef8ad25391c99fd90fc6ad5a485
ItsFRZ <faraz.ar.sheikh@gmail.com>
2021-12-30 23:09:22 +0530

    Leaders added

--- AngularOne/AngularOne/src/app/components/home/home.component.ts
+++ AngularOne/AngularOne/src/app/components/home/home.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {Dish} from '../../shared/dish';
 import {Promotion} from '../../shared/promotion';
 import { DishService } from 'src/app/services/dish.service'; 
-import { PromotionService } from 'src/app/services/promotion.service'; 
-
+import { PromotionService } from 'src/app/services/promotion.service';
+import { LeaderService } from 'src/app/services/leader.service';
+import {Leader} from 'src/app/shared/leader';
 
 @Component({
   selector: 'app-home',
@@ -12,15 +13,17 @@ import { PromotionService } from 'src/app/services/promotion.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private dishService:DishService, private promotionService: PromotionService) { }
+  constructor(private dishService:DishService, private promotionService: PromotionService, private leaderService : LeaderService) { }
 
   dish : Dish;
   promotion : Promotion;
+  leader : Leader;
 
   ngOnInit(): void {
 
     this.dish = this.dishService.getFeaturedDish();
     this.promotion = this.promotionService.getFeaturedPromotion();
+    this.leader = this.leaderService.getFeaturedLeader();
 
   }
 
---
+++ AngularOne/AngularOne/src/app/services/leader.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {Leader} from 'src/app/shared/leader';
+import {LEADERS} from 'src/app/shared/leaders';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LeaderService {
+
+  constructor() { }
+
+  getLeaders() : Leader[]{
+    return LEADERS;
+  }
+
+  getFeaturedLeader( ) : Leader{
+    return LEADERS.filter((leader) => leader.featured)[0];
+  }
+
+}
