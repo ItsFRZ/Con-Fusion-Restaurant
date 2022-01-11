9b5309677a743e09d0f2bf9a8d082dd2494dc715
ItsFRZ <faraz.ar.sheikh@gmail.com>
2022-01-12 00:48:33 +0530

    Template Driven Forms Part 1

--- AngularOne/src/app/app.module.ts
+++ AngularOne/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+
 import {MatListModule} from '@angular/material/list';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
 
 
 import {DishService} from './services/dish.service';
@@ -23,6 +29,7 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { LoginComponent } from './components/login/login.component';
+import { PopupComponent } from './components/popup/popup.component';
 
 @NgModule({
   declarations: [
@@ -34,18 +41,24 @@ import { LoginComponent } from './components/login/login.component';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    PopupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatListModule,
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule
+
   ],
   providers: [
     DishService,
@@ -53,7 +66,8 @@ import { LoginComponent } from './components/login/login.component';
   ],
   bootstrap: [AppComponent],
   entryComponents : [
-    LoginComponent
+    LoginComponent,
+    PopupComponent
   ]
 })
 export class AppModule { }
--- AngularOne/src/app/components/home/home.component.ts
+++ AngularOne/src/app/components/home/home.component.ts
@@ -5,6 +5,8 @@ import { DishService } from 'src/app/services/dish.service';
 import { PromotionService } from 'src/app/services/promotion.service';
 import { LeaderService } from 'src/app/services/leader.service';
 import {Leader} from 'src/app/shared/leader';
+import {MatDialog,MatDialogRef} from '@angular/material/dialog';
+import { PopupComponent } from '../popup/popup.component';
 
 @Component({
   selector: 'app-home',
@@ -13,18 +15,24 @@ import {Leader} from 'src/app/shared/leader';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private dishService:DishService, private promotionService: PromotionService, private leaderService : LeaderService) { }
+  constructor(private dishService:DishService, private promotionService: PromotionService, private leaderService : LeaderService,
+    public dialog : MatDialog) { }
 
   dish : Dish;
   promotion : Promotion;
   leader : Leader;
 
+  
   ngOnInit(): void {
 
     this.dish = this.dishService.getFeaturedDish();
     this.promotion = this.promotionService.getFeaturedPromotion();
     this.leader = this.leaderService.getFeaturedLeader();
 
+  }
+
+  popUpMenu() : void{
+    this.dialog.open(PopupComponent,{width : '500px' , height : '600px'});
   }
 
 }
